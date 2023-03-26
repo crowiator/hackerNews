5a8ec224f8b6559efac0c995914d58ad4132241c
Samuel Vrana <vrana.samuel@gmail.com>
2023-03-26 15:18:22 +0200

    Initial Commit

---
+++ hackerNews/hackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  hackerNewsApp.swift
+//  hackerNews
+//
+//  Created by crow on 26/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct hackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
